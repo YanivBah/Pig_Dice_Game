315f64fe772231b1bb63aabc548ee300ce0874a0
3agleFF67c1 <ybahalker@gmail.com>
2021-03-19 18:42:07 +0200

    Roll dice added

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,6 +5,7 @@ class Game extends React.Component {
   state = {
     winningScore: 100,
     currentPlayerTurn: 'player1',
+    rollDice: false,
     player1: {
       totalScore: 0,
       currentScore: 0,
@@ -42,10 +43,19 @@ class Game extends React.Component {
     }
   }
 
+  rollDice = async () => {
+    await this.state.rollDice
+      ? this.setState({ rollDice: false })
+      : this.setState({ rollDice: true });
+    if (!this.state.rollDice) {
+      setTimeout(() => {this.setState({ rollDice: true })},100)
+    }
+  }
+
   render() {
     return (
       <div>
-        <Dice onRender={this.updateCurrentScore} />
+        {this.state.rollDice && <Dice onRender={this.updateCurrentScore} />}
         <div>
           <h1>Player1:</h1>
           <h3>Total Score - {this.state.player1.totalScore}</h3>
@@ -56,6 +66,7 @@ class Game extends React.Component {
           <h3>Total Score - {this.state.player2.totalScore}</h3>
           <h3>Current Score - {this.state.player2.currentScore}</h3>
         </div>
+        <button onClick={this.rollDice}>Roll Dice</button>
       </div>
     );
   }
